4b9dd8fa81c30cffba892d04000eabddcf50be80
Priyesh <priyesh.raj9@gmail.com>
2022-03-29 21:43:27 +0530

    rebased commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,6 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductComponent } from './components/products/product/product.component';
 import { ItemCardComponent } from './components/products/item-card/item-card.component';
-
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card'
-import {MatButtonModule} from '@angular/material/button';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
 import { ProductDetailComponent } from './components/products/product-detail/product-detail.component';
 import { AddProductComponent } from './components/products/add-product/add-product.component';
 import { Error404Component } from './components/error404/error404.component';
